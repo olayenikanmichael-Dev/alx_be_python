a2e8795cb51c2d4040e44b5014aedc0359c29a20
olayenikanmichael-Dev <olayenikanmichaelolaniyi@gmail.com>
2025-11-04 19:10:39 +0100

    Done!!

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,1 +1,11 @@
-"" 
+number1 = 40,
+number2 = 300,
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Print results
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
